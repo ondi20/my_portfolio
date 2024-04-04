a9b1b5344abbf4cd16fea396987b470dfb559f76
Onyango Ondigo <138160676+ondi20@users.noreply.github.com>
2024-04-04 10:46:30 +0000

    just about page

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # sej_portfolio
+
+eu9zq0n8
+
+npm create sanity@latest -- --template get-started --project eu9zq0n8 --dataset production --provider github
---
+++ frontend_react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { About, Work } from './container';
+//import { Navbar } from './components';
+import './App.css';
+
+const App = () => (
+  <>
+  <div className="app">
+    <About />
+    <Work />
+  </div>
+  </>
+);
+
+export default App;
